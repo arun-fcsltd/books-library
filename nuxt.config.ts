552331912006de631bd135c4024e3465e8723a95
arun-fscltd <arusingh0726@gmail.com>
2025-09-25 10:35:53 +0530

    as

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,15 +32,6 @@ export default defineNuxtConfig({
   css:[
       'primeicons/primeicons.css',
   ],
- runtimeConfig: {
-    // private keys (server only)
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
 
-    // public keys (exposed to client)
-    public: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    },
-  },
   
 })
